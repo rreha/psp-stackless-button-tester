e8d7d50e3fd44d570c32299235656ab1889ffb19
reha <74599574+rreha@users.noreply.github.com>
2022-08-18 15:13:58 +0300

    Organized the code

--- script.py
+++ script.py
@@ -12,82 +12,50 @@ blank = psp2d.Color(0, 0, 0)
 # ^^^^^^ Somehow pad.analogX and Y works better than this??? (it prints back to "Press to a button" when you stop holding the button for some reason)
 ctrl = True
 
+def buttonprint(text):
+	img.clear(blank)
+	font.drawText(img, 0, 0, text)
+	screen.blit(img)
+	screen.swap()
+
+
 while ctrl == True:
 	pad = psp2d.Controller()
 	if pad.circle:
-		img.clear(blank)
-		font.drawText(img, 0, 0, "You pressed O")
-		screen.blit(img)
-		screen.swap()
+		buttonprint("You pressed O")
 
 	elif pad.cross:
-		img.clear(blank)
-		font.drawText(img, 0, 0, "You pressed X")
-		screen.blit(img)
-		screen.swap()
+		buttonprint("You pressed X")
 
 	elif pad.triangle:
-		img.clear(blank)
-		font.drawText(img, 0, 0, "You pressed /\\")
-		screen.blit(img)
-		screen.swap()
+		buttonprint("You pressed /\\")
 
 	elif pad.square:
-		img.clear(blank)
-		font.drawText(img, 0, 0, "You pressed []")
-		screen.blit(img)
-		screen.swap()
+		buttonprint("You pressed []")
 
 	elif pad.up:
-		img.clear(blank)
-		font.drawText(img, 0, 0, "You pressed UP D-PAD")
-		screen.blit(img)
-		screen.swap()
+		buttonprint("You pressed UP DPAD")
 
 	elif pad.down:
-		img.clear(blank)
-		font.drawText(img, 0, 0, "You pressed DOWN D-PAD")
-		screen.blit(img)
-		screen.swap()
+		buttonprint("You pressed DOWN DPAD")
 
 	elif pad.left:
-		img.clear(blank)
-		font.drawText(img, 0, 0, "You pressed LEFT D-PAD")
-		screen.blit(img)
-		screen.swap()
+		buttonprint("You pressed LEFT DPAD")
 
 	elif pad.right:
-		img.clear(blank)
-		font.drawText(img, 0, 0, "You pressed RIGHT D-PAD")
-		screen.blit(img)
-		screen.swap()
+		buttonprint("You pressed RIGHT DPAD")
 
 	elif pad.l:
-		img.clear(blank)
-		font.drawText(img, 0, 0, "You pressed LEFT TRIGGER")
-		screen.blit(img)
-		screen.swap()
+		buttonprint("You pressed LEFT TRIGGER")
 
 	elif pad.r:
-		img.clear(blank)
-		font.drawText(img, 0, 0, "You pressed RIGHT TRIGGER")
-		screen.blit(img)
-		screen.swap()
+		buttonprint("You pressed RIGHT TRIGGER")
 
 	elif pad.start:
-		img.clear(blank)
-		font.drawText(img, 0, 0, "You pressed START")
-		screen.blit(img)
-		screen.swap()
+		buttonprint("You pressed START")
 
 	elif pad.select:
-		img.clear(blank)
-		font.drawText(img, 0, 0, "You pressed SELECT")
-		screen.blit(img)
-		screen.swap()
+		buttonprint("You pressed SELECT")
 
 	elif pad.analogX or pad.analogY:
-		img.clear(blank)
-		font.drawText(img, 0, 0, "Press to a button")
-		screen.blit(img)
-		screen.swap()
+		buttonprint("Press to a button")
